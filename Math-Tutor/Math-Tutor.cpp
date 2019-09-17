9c49b486474bf30d3ff28b796c3eb0678659c82b
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-17 13:32:08 -0600

    Add project files.

---
+++ Math-Tutor/Math-Tutor.cpp
@@ -0,0 +1,18 @@
+// Jacob Hardman
+// Comp Sci 1
+// Professor Jennifer Bailey
+// 9/17/19
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int num1;
+	int num2;
+	int sum;
+
+    cout << "Hello World!\n";
+}
