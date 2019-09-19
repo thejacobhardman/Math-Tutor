39ddd15a3786517abded362da342984ff497161b
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-19 13:52:47 -0600

    added guess logic and formatted code.

--- Math-Tutor/Math-Tutor.cpp
+++ Math-Tutor/Math-Tutor.cpp
@@ -12,19 +12,16 @@ using namespace std;
 int main()
 {
 	bool isRunning = true;
-	string userInput;
-	string mode;
-	int num1;
-	int num2;
-	int answer;
+	string userInput, mode;
+	int num1, num2, answer;
 
 	while (isRunning == true) {
-		cout << "\nWelcome to 'Math Tutor!'";
+		cout << "Welcome to 'Math Tutor!'";
 
 		bool userConfirm = false;
 
 		while (userConfirm == false) {
-			cout << "\nPlease enter '1' for an addition problem, '2' for a subtraction problem, '3' for a multiplication problem, or '4' for a division problem: ";
+			cout << "\nPlease enter '1' for an addition problem, \n'2' for a subtraction problem, \n'3' for a multiplication problem, \nor '4' for a division problem: ";
 			cin >> userInput;
 
 			int selection = stoi(userInput);
@@ -57,33 +54,79 @@ int main()
 
 		// Addition problem
 		if (mode == "add") {
-			cout << "You selected an addition problem.";
+			cout << "\nYou selected an addition problem." << endl;
 			num1 = rand() % 999 + 1;
 			num2 = rand() % 999 + 1;
 			answer = num1 + num2;
 
-			cout << "\n" << num1 << " plus " << num2 << " = " << answer << ".";
+			cout << "\nSolve: ";
+			cout << "\n " << right << setw(7) << num1 << "\n+" << right << setw(7) << num2 << "\n--------\n     ";
+			
 			cin >> userInput;
+			int guess = stoi(userInput);
+
+			if (guess == answer) {
+				cout << "\nCORRECT!!!";
+			}
+			else {
+				cout << "\nINCORRECT!!!";
+			}
+
+			cout << "\n" << num1 << " plus " << num2 << " = " << answer << ".\n" << endl;
 		}
 		// Subtraction problem
 		else if (mode == "sub") {
-			cout << "You selected a subtraction problem.";
+			cout << "\nYou selected a subtraction problem."<< endl;
 			num1 = rand() % 999 + 1;
 			num2 = rand() % num1 + 1;
 			answer = num1 - num2;
 
-			cout << "\n" << num1 << " minus " << num2 << " = " << answer << ".";
+			cout << "\nSolve: ";
+			cout << "\n " << right << setw(7) << num1 << "\n-" << right << setw(7) << num2 << "\n--------\n     ";
+
 			cin >> userInput;
+			int guess = stoi(userInput);
+
+			if (guess == answer) {
+				cout << "\nCORRECT!!!";
+			}
+			else {
+				cout << "\nINCORRECT!!!";
+			}
+
+			cout << "\n" << num1 << " minus " << num2 << " = " << answer << ".\n" << endl;
 		}
 		// Multiplication problem
 		else if (mode == "mul") {
-			cout << "You selected a multiplication problem.";
+			cout << "\nYou selected a multiplication problem." << endl;
 			num1 = rand() % 999 + 1;
 			num2 = rand() % 999 + 1;
 			answer = num1 * num2;
+			string displayAnswer = to_string(answer);
+			int length = strlen(displayAnswer.c_str());
+			string formattedAnswer;
 
-			cout << "\n" << num1 << " times " << num2 << " = " << answer << ".";
+			for (int i = 0; i < length; i++) {
+				if (i % 3 == 0 && i != 0) {
+					formattedAnswer += ",";
+				}
+				formattedAnswer += displayAnswer.at(i);
+			}
+
+			cout << "\nSolve: ";
+			cout << "\n " << right << setw(7) << num1 << "\nX" << right << setw(7) << num2 << "\n--------\n   ";
+
 			cin >> userInput;
+			int guess = stoi(userInput);
+
+			if (guess == answer) {
+				cout << "\nCORRECT!!!";
+			}
+			else {
+				cout << "\nINCORRECT!!!";
+			}
+
+			cout << "\n" << num1 << " times " << num2 << " = " << formattedAnswer << ".\n" << endl;
 		}
 		// Division problem
 		else if (mode == "div") {
@@ -92,7 +135,7 @@ int main()
 			double divNum2 = rand() % 999 + 1.00;
 			answer = divNum1 / divNum2;
 
-			cout << "\n" << divNum1 << " divided by " << divNum2 << " = " << answer << ".";
+			cout << "\n" << divNum1 << " divided by " << divNum2 << " = " << answer << ".\n" << endl;
 			cin >> userInput;
 		}
 		// Catch-All
